e022b24fe3d446f1a2c2b693db0b122a3b32a73c
MathieuDub01s <161617711+MathieuDub01s@users.noreply.github.com>
2025-02-01 18:39:45 -0500

    update

--- js/script.js
+++ js/script.js
@@ -9,4 +9,8 @@ document.addEventListener("DOMContentLoaded", function(){
             }
         }
     });
+    fetch("http://127.0.0.1:5000/requests")
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error("Error:", error));
 });
